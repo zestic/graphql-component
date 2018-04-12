f5c8389a3cb2749907bf5e51f2780e933d57c108
iampersistent <shank@zestic.com>
2018-04-12 08:51:06 -0700

    tests for extract factory

--- src/Factory/ExtractJwtIntoContextFactory.php
+++ src/Factory/ExtractJwtIntoContextFactory.php
@@ -10,6 +10,13 @@ class ExtractJwtIntoContextFactory
 {
     public function __invoke(ContainerInterface $container): ExtractJwtIntoContext
     {
+        $config = $container->get('config');
+        $jwtConfig = $config['jwt'];
+        $publicKey = file_get_contents($jwtConfig['publicKeyPath']);
+        $options = [
+            'secret' => $publicKey,
+        ];
 
+        return new ExtractJwtIntoContext($options);
     }
 }
--- tests/unit/Factory/ExtractJwtIntoContextFactoryCest.php
+++ tests/unit/Factory/ExtractJwtIntoContextFactoryCest.php
@@ -3,12 +3,56 @@ declare(strict_types=1);
 
 namespace Tests\Unit\Factory;
 
+use ArrayObject;
+use Closure;
+use Tests\Fixture\TestContainer;
 use UnitTester;
+use Zestic\Factory\ExtractJwtIntoContextFactory;
 
 class ExtractJwtIntoContextFactoryCest
 {
+
     public function testInvoke(UnitTester $I)
     {
+        $container = new TestContainer();
+
+        $jwtConfig = [
+            'jwt' => [
+                'privateKeyPath' => __DIR__ . '../../_support/Fixture/jwt/private.pem',
+                'publicKeyPath' => __DIR__ . '../../_support/Fixture/jwt/public.pem',
+                'tokenTtl' => 3600,
+            ],
+        ];
+        $config = new ArrayObject($jwtConfig);
+        $container->set('config', $config);
+
+        $extractJwtIntoContext = (new ExtractJwtIntoContextFactory())->__invoke($container);
+        $getOptions = function () {
+            return $this->options;
+        };
+        $options = Closure::bind($getOptions, $extractJwtIntoContext, ExtractJwtIntoContextFactory::class)->__invoke();
+
         // secret should be set from jwt config location, loaded then passed
+        $I->assertSame($this->getExpectedPublicKey(), $options['secret']);
+    }
+
+    private function getExpectedPublicKey(): string
+    {
+        return <<<PUBLIC_KEY
+-----BEGIN PUBLIC KEY-----
+MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEApJ1O3NsW3oWAYgMouZyE
+yP8faYCm/P2l/hb1YaPEWiPCS6wI3E2gcDbSS/pQuBOZA8GX5xBYNG6G3s7KHwUi
+2Z1IlvaH0Ac+pZIVZDhgyerIn7JZVlzcGgq0NFvljr0P9kDZkNW2LLhLYVISQAcv
+Y04u1LlgjqPbWRzg5DXUwSdPwccnLGwsNLYJEhtmAUz+ggNpZGiiQkA8dO90Us+e
+OWWAKQPItkAsxTfiIBi5D3L3L4EG4UbsKcn7LJCvnQ+O9vCUmZlOWiPAJ3cN7eX3
+CqfZVZqaw+pq5UYAObBgJjRKi5z0xVS+JRCvpYPLz05ehK1ctklE1+32GGG8WdL2
+QfP9N1eJHqJmVzymA+jqdmoKzmEVeHkn1BPms6XkUTLyzipxcdNbVcXYHDDHfKar
+EgxATlitxSAk/UE+YqrN3WhIA7PCVi3SwohpSSJ8o1A+/zTe0eZ/gx4g1hf5LgTd
+PQDsK2G5piwGy1Xy+lKUZIBMnTKw9CrwVnG7KimkwvPBCxnxblFq3Gx8R9/CDitV
+ZkbOKxP7Q+18b+z7z/+EC4W7nPQa1wYR5dSPzyvPVAM23sNDInNoEVlHGa6jsnxe
+VQUlL1F88OnApUKeJ+rOck2P7+X8v/keHzueR1GtGCnjukNjQW1AvmSQBQCWReei
++SVgCN1jO75vpPjVUs22ZbMCAwEAAQ==
+-----END PUBLIC KEY-----
+PUBLIC_KEY;
     }
 }
