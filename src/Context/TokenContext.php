e4151c53d63b7678a802cb2e88356bd18ccfc98c
iampersistent <shank@zestic.com>
2020-11-25 22:34:42 -0800

    moved the decoding to the middleware

---
+++ src/Context/TokenContext.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+namespace IamPersistent\GraphQL\Context;
+
+use App\Jwt\JwtConfiguration;
+use Firebase\JWT\JWT;
+use GraphQL\Language\AST\DocumentNode;
+use GraphQL\Server\OperationParams;
+use IamPersistent\GraphQL\Middleware\RequestContextInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Log\LoggerInterface;
+use Psr\Log\LogLevel;
+
+final class TokenContext implements RequestContextInterface
+{
+    /** @var \Psr\Http\Message\ServerRequestInterface */
+    private $request;
+
+    public function __invoke(OperationParams $params, DocumentNode $doc, $operationType)
+    {
+        return $this->request->getAttribute('token');
+    }
+
+    public function setRequest(ServerRequestInterface $request)
+    {
+        $this->request = $request;
+    }
+}
