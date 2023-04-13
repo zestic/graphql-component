095b9b54d89aa32f967961631b7edc66c156e15d
Rich Shank <rich@zestic.com>
2023-04-12 21:32:24 -0500

    Update MessengerBusLocatorFactory for laminas-messenger

--- src/Locator/MessengerBusLocatorFactory.php
+++ src/Locator/MessengerBusLocatorFactory.php
@@ -4,25 +4,19 @@ declare(strict_types=1);
 namespace Zestic\GraphQL\Locator;
 
 use Laminas\ServiceManager\Factory\FactoryInterface;
-use Netglue\PsrContainer\Messenger\Container\MessageBusOptionsRetrievalBehaviour;
+use Netglue\PsrContainer\Messenger\Container\Util;
 use Netglue\PsrContainer\Messenger\HandlerLocator\OneToManyFqcnContainerHandlerLocator;
 use Psr\Container\ContainerInterface;
 
 final class MessengerBusLocatorFactory implements FactoryInterface
 {
-    use MessageBusOptionsRetrievalBehaviour;
-
-    /** @var string */
-    private $busIdentifier;
-
-    public function __construct(string $busIdentifier)
-    {
-        $this->busIdentifier = $busIdentifier;
-    }
+    public function __construct(
+        private string $busIdentifier,
+    ) { }
 
     public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
     {
-        $options = $this->options($container, $this->busIdentifier);
+        $options = Util::messageBusOptions($container, $this->busIdentifier);
 
         return new OneToManyFqcnContainerHandlerLocator($options->handlers(), $container);
     }
