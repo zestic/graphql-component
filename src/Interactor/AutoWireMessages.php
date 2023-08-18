d086c6ec0e1375f7794f346f3b3b8f8140382982
iampersistent <shank@zestic.com>
2023-08-18 08:55:41 -0500

    refactor to use php filesystem instead of Finder

--- src/Interactor/AutoWireMessages.php
+++ src/Interactor/AutoWireMessages.php
@@ -4,28 +4,20 @@ declare(strict_types=1);
 namespace Zestic\GraphQL\Interactor;
 
 use Composer\Autoload\ClassLoader;
-use Symfony\Component\Finder\Finder;
 use Zestic\GraphQL\GraphQLMutationMessageInterface;
 use Zestic\GraphQL\GraphQLQueryMessageInterface;
 
 class AutoWireMessages
 {
-    private static string $interface;
-    private static string $contentFilter;
+    private static array $files;
 
     public static function findHandlersForInterface(string $interface): array
     {
-        self::$interface = $interface;
-        self::$contentFilter = "/" . (new \ReflectionClass($interface))->getShortName() . "/";
+        self::loadFilePaths();
 
-        $loaders = ClassLoader::getRegisteredLoaders();
-        $namespaces = [];
-        foreach ($loaders as $loader) {
-            $namespaces = array_merge($namespaces, $loader->getPrefixesPsr4());
-        }
-        $messages = self::findMessagesForInterface($namespaces);
+        $messages = self::findMessagesForInterface($interface);
 
-        return self::findHandlersForMessages($messages, $namespaces);
+        return self::findHandlersForMessages($messages);
     }
 
     public static function getMutationHandlers(): array
@@ -57,25 +49,21 @@ class AutoWireMessages
         return in_array($interface, class_implements($classname));
     }
 
-    private static function findHandlersInDirectories(string $message, array $directories): array
+    private static function findHandlersForMessage(string $message): array
     {
         $handlers = [];
-        foreach ($directories as $directory) {
-            if (!is_dir($directory)) {
-                continue;
-            }
-            $finder = new Finder();
-            $finder
-                ->in($directory)
-                ->name('*.php');
-            foreach ($finder->files()->contains($message) as $fileInfo) {
+        foreach (self::$files as $index => $file) {
+            $content = file_get_contents($file);
+            if (str_contains($content, $message)) {
+                $classname = self::getFQCNFromFile($file);
                 try {
-                    $classname = self::getFQCNFromFileInfo($fileInfo);
                     if (self::classHandlesMessage($classname, $message)) {
                         $handlers[] = $classname;
                     }
                 } catch (\Exception $e) {
-                    continue;
+                } finally {
+                    // remove the file to save time looping
+                    unset(self::$files[$index]);
                 }
             }
         }
@@ -83,85 +71,44 @@ class AutoWireMessages
         return $handlers;
     }
 
-    /**
-     * @param string[] $messages
-     * @param array $namespaces
-     *
-     * @return string[]
-     */
-    private static function findHandlersForMessages(array $messages, array $namespaces): array
+    private static function findHandlersForMessages(array $messages): array
     {
-        $directories = [];
-        foreach ($namespaces as $namespaceDirectories) {
-            $directories = array_merge($directories, $namespaceDirectories);
-        }
         $handlers = [];
         foreach ($messages as $message) {
-            $handlers[$message] = self::findHandlersInDirectories($message, $directories);
+            $handlers[$message] = self::findHandlersForMessage($message);
         }
 
         return $handlers;
     }
 
-    private static function findMessagesForInterface(array $namespaces): array
+    private static function findMessagesForInterface(string $interface): array
     {
         $messages = [];
-        foreach ($namespaces as $namespace => $directories) {
-            $messages = array_merge($messages, self::findMessagesInNamespace($namespace, $directories));
-        }
-
-        return $messages;
-    }
-
-    private static function findMessagesInNamespace(string $namespace, array $directories): array
-    {
-        $messages = [];
-        foreach ($directories as $directory) {
-            if (!is_dir($directory)) {
-                continue;
-            }
-            $finder = new Finder();
-            $finder
-                ->in($directory)
-                ->name('*.php')
-                ->depth('== 0');
-            foreach ($finder->files()->contains(self::$contentFilter) as $fileInfo) {
-                try {
-                    $classname = self::getFQCNFromFileInfo($fileInfo);
-                    if (self::classImplementsInterface($classname, self::$interface)) {
-                        $messages[] = $classname;
-                    }
-                } catch (\Exception $e) {
-                    continue;
+        foreach (self::$files as $index => $file) {
+            $content = file_get_contents($file);
+            if (str_contains($content, $interface)) {
+                $classname = self::getFQCNFromFile($file);
+                if (self::classImplementsInterface($classname, $interface)) {
+                    $messages[] = $classname;
+                    unset(self::$files[$index]);
                 }
             }
-            $finder = new Finder();
-            // find all files in the current directory
-            $finder
-                ->in($directory)
-                ->depth('== 0');
-            foreach ($finder->directories() as $subDirectory) {
-                $subDirectoryMessages = self::findMessagesInNamespace(
-                    "{$namespace}{$subDirectory->getRelativePathname()}\\",
-                    [$subDirectory->getPathname()]
-                );
-                $messages = array_merge($messages, $subDirectoryMessages);
-            }
         }
 
         return $messages;
     }
 
-    private static function getFQCNFromFileInfo(\SplFileInfo $fileInfo): string
+    private static function getFQCNFromFile(string $filePath): string
     {
-        $namespace = self::getNamespaceFromFile($fileInfo);
+        $namespace = self::getNamespaceFromFile($filePath);
+        $pathParts = pathinfo($filePath);
 
-        return $namespace.'\\'.$fileInfo->getBasename('.php');
+        return $namespace.'\\'.$pathParts['filename'];
     }
 
-    private static function getNamespaceFromFile(\SplFileInfo $fileInfo): string
+    private static function getNamespaceFromFile(string $filePath): string
     {
-        $tokens = self::getTokensFromFile($fileInfo);
+        $tokens = self::getTokensFromFile($filePath);
         $namespaceStart = false;
         foreach ($tokens as $token) {
             if (is_array($token) && $token[0] == T_NAMESPACE) {
@@ -176,9 +123,9 @@ class AutoWireMessages
         return '';
     }
 
-    private static function getTokensFromFile(\SplFileInfo $fileInfo): array
+    private static function getTokensFromFile(string $filePath): array
     {
-        $fp = fopen($fileInfo->getRealPath(), 'r');
+        $fp = fopen($filePath, 'r');
         $buffer = '';
         while (!feof($fp)) {
             $buffer .= fread($fp, 512);
@@ -186,5 +133,43 @@ class AutoWireMessages
         fclose($fp);
 
         return token_get_all($buffer);
+    }
+
+    private static function loadFilePaths(): void
+    {
+        $loaders = ClassLoader::getRegisteredLoaders();
+        $namespaces = [];
+        foreach ($loaders as $loader) {
+            $namespaces = array_merge($namespaces, $loader->getPrefixesPsr4());
+        }
+
+        foreach ($namespaces as $namespace => $directories) {
+            self::scanDirectories($namespace, $directories);
+        }
+    }
+
+    private static function scanDirectories(string $namespace, array $directories): void
+    {
+        $subDirectories = [];
+        foreach ($directories as $directory) {
+            if (is_dir($directory)) {
+                if ($dh = opendir($directory)) {
+                    while (($file = readdir($dh)) !== false) {
+                        $filePath = $directory . '/' . $file;
+                        $info = pathinfo($filePath);
+                        if ($info['extension'] === 'php') {
+                            self::$files[] = realpath($filePath);
+                        };
+                        if ($info['basename'] === $info['filename']) {
+                            $subDirectories[] = $filePath;
+                        }
+                    }
+                    closedir($dh);
+                }
+            }
+        }
+        if (!empty($subDirectories)) {
+            self::scanDirectories($namespace, $subDirectories);
+        }
     }
 }
